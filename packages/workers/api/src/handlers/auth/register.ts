4ed438e5beed746e58a80a242678a9a40e65e86d
Hideki Tanaka <work@tuxsnct.com>
2022-04-29 12:20:22 +0900

    update @tuxsnct/api-worker to v1.5.0

--- packages/workers/api/src/handlers/auth/register.ts
+++ packages/workers/api/src/handlers/auth/register.ts
@@ -1,4 +1,5 @@
-import { User, getJwtTokens, generateCookie, generateSecret } from '@tuxsnct/workers-module-auth'
+import { Validator, Schema } from '@cfworker/json-schema'
+import { getJwtTokens, generateCookie, generateSecret, User } from '@tuxsnct/workers-module-auth'
 import { Handler } from 'worktop'
 import { KV, read, write } from 'worktop/kv'
 import { uid } from 'worktop/utils'
@@ -6,23 +7,36 @@ import { uid } from 'worktop/utils'
 // eslint-disable-next-line init-declarations
 declare let USERS: KV.Namespace
 
+const schemaRegister: Schema = {
+  properties: {
+    email: { type: 'string' },
+    password: { type: 'string' }
+  },
+  required: ['email', 'password'],
+  type: 'object'
+}
+
 // eslint-disable-next-line max-statements
 export const handleRegister: Handler = async (request, response) => {
   let { code, data, headers }: ResponseItems = { code: 401, data: {}, headers: { 'content-type': 'application/json' } }
+  const body = await request.body() as User
 
-  const user = await request.body() as User
-  user.id = uid()
-  const secret = generateSecret().toUpperCase()
-  user.secret = secret
+  if (new Validator(schemaRegister).validate(body)) {
+    const user = body
+    user.id = uid()
+    const secret = generateSecret()
+    user.secret = secret
 
-  if (!(await read(USERS, user.email)) && await write(USERS, user.email, JSON.stringify(user))) {
-    const jwtTokens = await getJwtTokens(user)
+    if (!(await read(USERS, user.email)) && await write(USERS, user.email, JSON.stringify(user))) {
+      const jwtTokens = await getJwtTokens(user)
 
-    if (jwtTokens) {
-      code = 200
-      data = { secret, token: jwtTokens.csrfToken }
-      headers['set-cookie'] = generateCookie({ name: 'token', value: jwtTokens.cookieToken })
+      if (jwtTokens) {
+        code = 200
+        data = { secret, token: jwtTokens.csrfToken }
+        headers['set-cookie'] = generateCookie({ name: 'token', value: jwtTokens.cookieToken })
+      }
     }
   }
+
   response.send(code, data, headers)
 }
